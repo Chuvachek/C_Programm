a4c98bd9b0bbe1bd327d719ee85bac9678c810c3
Yak_tsuk_tsope <defenzer.orm@gmail.com>
2026-05-08 21:01:30 +0300

    Learn C Farentheit
    Learn C Farentheit

--- Fahrenheit.c
+++ Fahrenheit.c
@@ -16,7 +16,7 @@
      fahr = lower;
      while (fahr <= upper) {
          celsius = 5 * (fahr-32) / 9;
-        printf ("%d\t%d\n", fahr, celsius);
+        printf ("%d %d\n", fahr, celsius);
          fahr = fahr + step;
      }
- }
+ }  
